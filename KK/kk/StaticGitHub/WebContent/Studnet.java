603ba1e86cc9ab1e8c89c8e7d78190d692496c06
wjc604584836 <66284058+wjc604584836@users.noreply.github.com>
2020-06-02 10:09:57 +0800

    Create Studnet.java

---
+++ KK/kk/StaticGitHub/WebContent/Studnet.java
@@ -0,0 +1,15 @@
+public Class Studnet{
+  private int id;
+  private String name;
+  private String arrdess;
+  
+  public Student(int id,String name,String arrdess){
+    this.id = id;
+    this.name = name;
+    this.arrdess = arrdess;
+  }
+  
+  public Student(){
+    
+  }
+}
